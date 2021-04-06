8107f2f155d93780e8f697388169a9f5154aa758
tstocker <thomas.stocker.pro@gmail.com>
2021-04-06 17:40:51 +0200

    [UPD] add cors allowing

--- index.php
+++ index.php
@@ -4,7 +4,15 @@ spl_autoload_register(function ($class_name) {
     include './src/'.$class_name . '.php';
 });
 
-header('Content-Type: application/json');
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+}
 
 $url = $_GET['url'];
 
